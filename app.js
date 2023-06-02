9dfbe6faae9028d7574eb6563f53e1f048fbfaeb
yongman park <yongman514@gmail.com>
2023-06-03 00:31:28 +0900

    login with localstorage

--- app.js
+++ app.js
@@ -3,21 +3,34 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-
+const USERNAME_KEY = "username";
 //const link = document.querySelector("a");
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add("hidden");
     const username = loginInput.value;
-    console.log(username);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    //information is from form, input
+    paintGreeting(username);
 }
 
 function handleLinkClick(){
     alert('click');
 }
 
+function paintGreeting(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
 //link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername === null) { 
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+    //information is from localstorage 
+    paintGreeting(savedUsername);
+}  
